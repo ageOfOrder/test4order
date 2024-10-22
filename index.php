1c032ee8d937509db9a1a103eaac01e57460f7c5
testOfChaos <serlesfrank@gmail.com>
2024-10-21 20:22:17 -0400

    8:22pm

--- index.php
+++ index.php
@@ -7,11 +7,33 @@ include('./var/varOne.php'); // variables that all pages should possess
 include('./var/postVar.php'); // if stuff gets posted we capture and clean it here
 include('./var/indexStates.php'); // switch statement
 
+// start the html page
 include('./handf/h.php');
 
+echo '<div class="pageTitle">'; // center it, etc.
 echo '<h1>index.php</h1>';
+echo '</div>';
+
+echo '<div class="topMenu">'; // menu buttons
+echo '<p class="debug">Index • New • Edit • Delete</p>';
+echo '</div>'; // end topMenu
+
+echo '<div class="leftMatter">'; // start leftMatter
+echo '<h2>Left</h2>';
+echo '</div>'; // end leftMatter
+
+echo '<div class="indexBody">'; // middle of the page
 echo '<p>This is a paragraph.</p>';
+echo '</div>'; // end indexBody
 
+echo '<div class="rightMatter">'; // start rightMatter
+echo '<h2>Right</h2>';
+echo '</div>'; // end rightMatter
+
+echo '<div class="bottomBar">';
+echo '<h2>Bottom bar</2>';
+echo '</div>'; // end bottomBar
+
+// end the html page and unset the title because it is funny
 include('./handf/f.php');
-
 ?>
